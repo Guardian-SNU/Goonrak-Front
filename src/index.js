c3c075052a1859b36d1a22a1b9e328ed5616c00a
dnsdhrj <dnsdhrj123@gmail.com>
2017-09-27 14:29:51 +0900

    Add router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import Home from './views/Home';
 import './main.css';
 
+// <Header /> should go below <BrowserRouter>
 ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={Home}/>
+    </Switch>
+  </BrowserRouter>
+  ,document.getElementById('root')
 );
